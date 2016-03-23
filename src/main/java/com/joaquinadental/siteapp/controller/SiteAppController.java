a4f05c4dc04320487dba49403dc882225c6ee3ae
Ratnadeep <ratnadeep.ms@outlook.com>
2016-03-23 01:16:26 -0500

    Initial Commit With basic structure

---
+++ src/main/java/com/joaquinadental/siteapp/controller/SiteAppController.java
@@ -0,0 +1,23 @@
+package com.joaquinadental.siteapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SiteAppController {
+	String message = "Welcome to Joaquina Dental :";
+	 
+	@RequestMapping("/hello")
+	public ModelAndView showMessage(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		System.out.println("in controller");
+ 
+		ModelAndView mv = new ModelAndView("helloworld");
+		mv.addObject("message", message);
+		mv.addObject("name", name);
+		return mv;
+	}
+
+}
